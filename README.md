ff732f415a2c709b751fd9b7da206f8ceb5d9eb9
Christian Hansen <mail@hansen-net.de>
2024-08-16 16:31:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dj-videoflix
+ Backend
---
+++ videoflix/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VideoflixConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'videoflix'
