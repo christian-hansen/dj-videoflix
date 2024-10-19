14ee493e45b8e05ff500ed1866480084c422ac31
Christian Hansen <mail@hansen-net.de>
2024-10-19 13:38:19 +0200

    Update settings.py

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-dl_51z1d%pv29ttlk^m!+gg-i9cxc)n25p&)!-^p4%t--^d(-2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '34.91.91.141', 'videoflix-backend.christian-hansen.dev', '186.209.244.88']
-CORS_ALLOW_ALL_ORIGINS = ['127.0.0.1', 'localhost', '34.91.91.141', 'videoflix-backend.christian-hansen.dev', '186.209.244.88']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '35.246.145.159 ', 'videoflix-backend.christian-hansen.dev', '186.209.244.88']
+CORS_ALLOW_ALL_ORIGINS = ['127.0.0.1', 'localhost', '35.246.145.159 ', 'videoflix-backend.christian-hansen.dev', '186.209.244.88']
 
 CACHE_TTL = 60 * 15
 
